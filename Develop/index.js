738078050950e86a679cc94898aa339e43842e3f
Christine Nguyen <cnhnguyen59@gmail.com>
2022-01-06 17:47:33 -0500

    created list of questions for generator to ask user

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,15 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is your Github username?',
+                    'What is your email address',
+                    'What is your project name?',
+                    'Please provide a short description of your project',
+                    'What command should be run to install dependencies?',
+                    'Please provide instructions/examples for use',
+                    'What kind of license should your project have?',
+                    'What does the user need to know about contributing?',
+                    'Were there any contributors to the project?'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
